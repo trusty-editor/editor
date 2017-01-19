a1eff52b6484bd40d6f2ae6f5f9b857991a9a521
BookOwl <stanleybookowl@gmail.com>
2017-01-19 16:51:24 -0500

    Start working on the editor struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+extern crate trusty_backend as backend;
+extern crate trusty_frontend as frontend;
+extern crate trusty_utils as utils;
+
+pub struct Editor<T: frontend::FrontEnd> {
+    backend: backend::BackEnd,
+    frontend: T,
+}
+
+impl<T> Editor<T> where T: frontend::FrontEnd {
+    pub fn new<K>(backend: backend::BackEnd, frontend: K) -> Editor<K> 
+                  where K: frontend::FrontEnd {
+        Editor { backend: backend, frontend: frontend }
+    }
+    // TODO
+    pub fn start(&self) where T: frontend::FrontEnd {
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
